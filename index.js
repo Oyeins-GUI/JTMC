e89c8083eabfd8f41b4b4c59cf40f9f15f0f9bd6
Oyeins-GUI <oyeinx7@gmail.com>
2022-06-27 19:04:45 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const headerLinks = document.querySelector(".header-links");
 const toggleBtn = document.querySelector(".toggle-btn");
 const links = document.querySelectorAll(".link");
+const date = document.querySelector(".date");
+
+date.innerHTML = new Date().getFullYear();
 
 toggleBtn.addEventListener("click", () => {
    headerLinks.classList.toggle("hide");
