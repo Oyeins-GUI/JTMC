30027765157595b2a676f338a45a005595ba5889
Oyeins-GUI <oyeinx7@gmail.com>
2022-06-28 18:38:12 +0100

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const navLinks = document.querySelector(".links");
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelectorAll(".links");
 
+// set date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
 navToggle.addEventListener("click", () => {
    navLinks.classList.toggle("show-links");
 });
